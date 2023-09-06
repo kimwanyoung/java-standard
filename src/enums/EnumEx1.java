76d83784b14969abe8ee528b83e5382dbb56e4b8
kimwanyoung <dhks2869@gmail.com>
2023-09-06 16:20:10 +0900

    feat : 스트림 정렬 예제

--- src/enums/EnumEx1.java
+++ src/enums/EnumEx1.java
@@ -1,36 +1,42 @@
 package enums;
 
+import java.util.Arrays;
+import java.util.List;
+
 enum Direction1 {EAST, SOUTH, WEST, NORTH}
 
 public class EnumEx1 {
 	public static void main(String[] args) {
-		Direction1 d1 = Direction1.EAST;
-		Direction1 d2 = Direction1.valueOf("WEST");
-		Direction1 d3 = Enum.valueOf(Direction1.class, "EAST");
+		// Direction1 d1 = Direction1.EAST;
+		// Direction1 d2 = Direction1.valueOf("WEST");
+		// Direction1 d3 = Enum.valueOf(Direction1.class, "EAST");
+		//
+		// System.out.println("d1 = " + d1);
+		// System.out.println("d2 = " + d2);
+		// System.out.println("d3 = " + d3);
+		//
+		// System.out.println("d1 == d2 ? " + (d1 == d2));
+		// System.out.println("d1 == d3 ? " + (d1 == d3));
+		// System.out.println("d1.equals(d3) ? " + d1.equals(d3));
+		// // System.out.println("d2 > d3 ? " + (d2 > d3)); Error
+		// System.out.println("d1.compareTo(d3) ? " + (d1.compareTo(d3)));
+		// System.out.println("d1.compareTo(d2) ? " + (d1.compareTo(d2)));
+		//
+		// switch (d1) {
+		// 	case EAST -> System.out.println("The direction is East.");
+		// 	case SOUTH -> System.out.println("The direction is South.");
+		// 	case WEST -> System.out.println("The direction is West.");
+		// 	case NORTH -> System.out.println("The direction is North.");
+		// 	default -> System.out.println("Invalid direction.");
+		// }
+		//
+		// Direction1[] dArr = Direction1.values();
+		//
+		// for (Direction1 d : dArr) {
+		// 	System.out.println(d.name() + " = " + d.ordinal());
+		// }
 
-		System.out.println("d1 = " + d1);
-		System.out.println("d2 = " + d2);
-		System.out.println("d3 = " + d3);
+		List<Integer> a = Arrays.asList(1, 2, null);
 
-		System.out.println("d1 == d2 ? " + (d1 == d2));
-		System.out.println("d1 == d3 ? " + (d1 == d3));
-		System.out.println("d1.equals(d3) ? " + d1.equals(d3));
-		// System.out.println("d2 > d3 ? " + (d2 > d3)); Error
-		System.out.println("d1.compareTo(d3) ? " + (d1.compareTo(d3)));
-		System.out.println("d1.compareTo(d2) ? " + (d1.compareTo(d2)));
-
-		switch (d1) {
-			case EAST -> System.out.println("The direction is East.");
-			case SOUTH -> System.out.println("The direction is South.");
-			case WEST -> System.out.println("The direction is West.");
-			case NORTH -> System.out.println("The direction is North.");
-			default -> System.out.println("Invalid direction.");
-		}
-
-		Direction1[] dArr = Direction1.values();
-
-		for (Direction1 d : dArr) {
-			System.out.println(d.name() + " = " + d.ordinal());
-		}
 	}
 }
---
+++ src/stream/StreamEx1.java
@@ -0,0 +1,52 @@
+package stream;
+
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class StreamEx1 {
+	public static void main(String[] args) {
+		Stream<Student> studentStream = Stream.of(new Student("이자바", 3, 300),
+			new Student("김자바", 3, 200),
+			new Student("안자바", 1, 100),
+			new Student("최자바", 2, 150),
+			new Student("박자바", 2, 210),
+			new Student("나자바", 1, 290),
+			new Student("홍자바", 1, 180));
+
+		studentStream.sorted(Comparator.comparing(Student::getBan).thenComparing(Comparator.naturalOrder()))
+			.forEach(System.out::println);
+	}
+}
+
+class Student implements Comparable<Student> {
+	String name;
+	int ban;
+	int totalScore;
+
+	Student(String name, int ban, int totalScore) {
+		this.name = name;
+		this.ban = ban;
+		this.totalScore = totalScore;
+	}
+
+	public String toString() {
+		return String.format("[%s, %d, %d]", name, ban, totalScore);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getBan() {
+		return ban;
+	}
+
+	public int getTotalScore() {
+		return totalScore;
+	}
+
+	@Override
+	public int compareTo(Student s) {
+		return s.totalScore - this.totalScore;
+	}
+}
