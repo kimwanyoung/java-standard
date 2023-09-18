1e50f18226aa68587e1c02bdfc7816019d0c49c4
kimwanyoung <dhks2869@gmail.com>
2023-09-18 15:28:19 +0900

    feat : 데몬 쓰레드

--- src/thread/ThreadEx10.java
+++ src/thread/ThreadEx10.java
@@ -1,7 +1,6 @@
 package thread;
 
 public class ThreadEx10 implements Runnable {
-
 	static boolean autoSave = false;
 
 	public static void main(String[] args) {
@@ -10,18 +9,20 @@ public class ThreadEx10 implements Runnable {
 		t.setDaemon(true);
 		t.start();
 
-		for (int i = 1; i <= 10; i++) {
+		for (int i = 0; i <= 10; i++) {
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 			}
+
 			System.out.println(i);
 
 			if (i == 5) {
 				autoSave = true;
 			}
 		}
-		System.out.println("프로그램을 종료합니다");
+
+		System.out.println("프로그램을 종료합니다.");
 	}
 
 	@Override
@@ -30,6 +31,7 @@ public class ThreadEx10 implements Runnable {
 			try {
 				Thread.sleep(3 * 1000);
 			} catch (InterruptedException e) {
+
 			}
 
 			if (autoSave) {
@@ -39,7 +41,6 @@ public class ThreadEx10 implements Runnable {
 	}
 
 	public void autoSave() {
-		System.out.println("작업 파일이 자동 저장되었습니다.");
+		System.out.println("작업파일이 자동 저장 되었습니다.");
 	}
-
 }
